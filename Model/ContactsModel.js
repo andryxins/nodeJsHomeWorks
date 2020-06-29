52aadeeb6cc8557c80d793568a20a61f7d53d268
andryxins <andryxins@icloud.com>
2020-06-29 20:46:01 +0300

    rewrited server settings to class, created new file with server settings

--- Model/ContactsModel.js
+++ Model/ContactsModel.js
@@ -5,75 +5,75 @@ const { promises: fsPromises } = fs;
 const contactsPath = path.join(process.cwd() + '/db/contacts.json');
 
 class ContactsModel {
-  listContacts = async () => {
-    const contactsList = await fsPromises.readFile(contactsPath, 'utf-8');
+    listContacts = async () => {
+        const contactsList = await fsPromises.readFile(contactsPath, 'utf-8');
 
-    return JSON.parse(contactsList);
-  };
-
-  getContactById = async contactId => {
-    const contactsList = await this.listContacts();
-    const targetContact = contactsList.find(user => user.id === contactId);
-
-    return targetContact;
-  };
-
-  removeContact = async contactId => {
-    const contactsList = await this.listContacts();
-
-    const targetContactIdx = contactsList.findIndex(
-      contact => contact.id === contactId,
-    );
-
-    if (targetContactIdx === -1) {
-      return false;
-    }
-
-    contactsList.splice(targetContactIdx, 1);
-
-    await fsPromises.writeFile(contactsPath, JSON.stringify(contactsList));
-
-    return true;
-  };
-
-  addContact = async candidateContact => {
-    const contactsList = await this.listContacts();
-
-    const newContact = {
-      ...candidateContact,
-      id: Math.floor(Math.random() * 10000) + '',
+        return JSON.parse(contactsList);
     };
 
-    await fsPromises.writeFile(
-      contactsPath,
-      JSON.stringify([...contactsList, newContact]),
-    );
+    getContactById = async contactId => {
+        const contactsList = await this.listContacts();
+        const targetContact = contactsList.find(user => user.id === contactId);
 
-    return newContact;
-  };
-
-  updateContact = async (id, updatedFields) => {
-    const contactsList = await this.listContacts();
-
-    const targetContactIdx = contactsList.findIndex(
-      contact => contact.id === id,
-    );
-
-    if (targetContactIdx === -1) {
-      return null;
-    }
-
-    const updatedContact = {
-      ...contactsList[targetContactIdx],
-      ...updatedFields,
+        return targetContact;
     };
 
-    contactsList[targetContactIdx] = updatedContact;
+    removeContact = async contactId => {
+        const contactsList = await this.listContacts();
 
-    await fsPromises.writeFile(contactsPath, JSON.stringify(contactsList));
+        const targetContactIdx = contactsList.findIndex(
+            contact => contact.id === contactId,
+        );
 
-    return updatedContact;
-  };
+        if (targetContactIdx === -1) {
+            return false;
+        }
+
+        contactsList.splice(targetContactIdx, 1);
+
+        await fsPromises.writeFile(contactsPath, JSON.stringify(contactsList));
+
+        return true;
+    };
+
+    addContact = async candidateContact => {
+        const contactsList = await this.listContacts();
+
+        const newContact = {
+            ...candidateContact,
+            id: Math.floor(Math.random() * 10000) + '',
+        };
+
+        await fsPromises.writeFile(
+            contactsPath,
+            JSON.stringify([...contactsList, newContact]),
+        );
+
+        return newContact;
+    };
+
+    updateContact = async (id, updatedFields) => {
+        const contactsList = await this.listContacts();
+
+        const targetContactIdx = contactsList.findIndex(
+            contact => contact.id === id,
+        );
+
+        if (targetContactIdx === -1) {
+            return null;
+        }
+
+        const updatedContact = {
+            ...contactsList[targetContactIdx],
+            ...updatedFields,
+        };
+
+        contactsList[targetContactIdx] = updatedContact;
+
+        await fsPromises.writeFile(contactsPath, JSON.stringify(contactsList));
+
+        return updatedContact;
+    };
 }
 
 module.exports = new ContactsModel();
--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const contactsRoutes = require('./Api/ContactsRoutes/ContactsRoutes');
+const server = require('./Server');
 
-const app = express();
-
-// middlewares
-
-app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms'),
-);
-
-// routes
-
-app.use('/api', contactsRoutes);
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log('Server started listening on port', PORT);
-});
+server.start()
